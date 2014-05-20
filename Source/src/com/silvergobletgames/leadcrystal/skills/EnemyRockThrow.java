6795e897b341b9d4d65e161406608301c3785112
Michael Signorella <michael@silvergobletgames.com>
2014-05-19 20:42:22 -0400

    sandman slowing goo

--- Source/src/com/silvergobletgames/leadcrystal/skills/EnemyRockThrow.java
+++ Source/src/com/silvergobletgames/leadcrystal/skills/EnemyRockThrow.java
@@ -2,28 +2,30 @@
 package com.silvergobletgames.leadcrystal.skills;
 
 import com.silvergobletgames.leadcrystal.combat.CombatEffect;
-import com.silvergobletgames.leadcrystal.entities.CombatEntity;
-import com.silvergobletgames.leadcrystal.entities.HitBox;
-import com.silvergobletgames.leadcrystal.entities.Entity;
-import com.silvergobletgames.leadcrystal.entities.WorldObjectEntity;
-import com.silvergobletgames.sylver.core.Scene.Layer;
-import com.silvergobletgames.sylver.graphics.Color;
-import com.silvergobletgames.sylver.graphics.Image;
-import com.silvergobletgames.sylver.graphics.AbstractParticleEmitter;
-import java.util.Random;
-import net.phys2d.math.Vector2f;
-import net.phys2d.raw.Body;
-import net.phys2d.raw.CollisionEvent;
-import net.phys2d.raw.StaticBody;
-import net.phys2d.raw.shapes.Box;
 import com.silvergobletgames.leadcrystal.combat.Damage;
 import com.silvergobletgames.leadcrystal.combat.Damage.DamageType;
 import com.silvergobletgames.leadcrystal.combat.StateEffect;
 import com.silvergobletgames.leadcrystal.core.ExtendedImageAnimations;
 import com.silvergobletgames.leadcrystal.core.LeadCrystalParticleEmitters.GreenGooEmitter;
+import com.silvergobletgames.leadcrystal.entities.CombatEntity;
+import com.silvergobletgames.leadcrystal.entities.Entity;
+import com.silvergobletgames.leadcrystal.entities.EntityEffect;
+import com.silvergobletgames.leadcrystal.entities.HitBox;
+import com.silvergobletgames.leadcrystal.entities.PlayerEntity;
+import com.silvergobletgames.leadcrystal.entities.WorldObjectEntity;
+import com.silvergobletgames.sylver.core.Scene.Layer;
+import com.silvergobletgames.sylver.graphics.AbstractParticleEmitter;
+import com.silvergobletgames.sylver.graphics.Color;
+import com.silvergobletgames.sylver.graphics.Image;
 import com.silvergobletgames.sylver.util.SylverRandom;
 import com.silvergobletgames.sylver.util.SylverVector2f;
+import java.util.Random;
 import net.phys2d.math.ROVector2f;
+import net.phys2d.math.Vector2f;
+import net.phys2d.raw.Body;
+import net.phys2d.raw.CollisionEvent;
+import net.phys2d.raw.StaticBody;
+import net.phys2d.raw.shapes.Box;
 import net.phys2d.raw.shapes.Circle;
 
 /**
@@ -51,14 +53,13 @@ public class EnemyRockThrow extends Skill
         Body body = new Body(new Circle(20), 3);
         Image img = new Image("plantSpit.png");
         img.setDimensions(40,40);
-        img.setColor(new Color(.5f,.5f,.5f,1f));
-        GooBombHitBox goo = new GooBombHitBox(damage, body, img, user);
+        img.setColor(new Color(.1f,.1f,.1f,1f));
+        EnemyTarHitBox goo = new EnemyTarHitBox(damage, body, img, user);
         goo.getBody().setGravityEffected(true);
         
         
         //Determine vector to target
         SylverVector2f distanceVector = user.distanceVector(user.getTarget());
-        System.err.println(distanceVector);
         distanceVector.normalise();
         
          distanceVector.add(new SylverVector2f(0,1f));
@@ -93,9 +94,9 @@ public class EnemyRockThrow extends Skill
     }
     
     
-    private class GooBombHitBox extends HitBox
+    private class EnemyTarHitBox extends HitBox
     {
-         public GooBombHitBox(Damage d, Body b, Image i, Entity user)
+         public EnemyTarHitBox(Damage d, Body b, Image i, Entity user)
          { 
             super(d, b, i, user); 
                       
@@ -108,7 +109,20 @@ public class EnemyRockThrow extends Skill
              //remove if we hit a world object
              if(other instanceof WorldObjectEntity )
              {
-                             
+                          
+                
+                //if we landed on the top of a worldObjectEntity
+                if(-event.getNormal().getY() > .75 )
+                {
+                     //put slowing tar puddle in the world
+                    HitBox slowingTar = new SlowingTarHitBox(user);
+                    slowingTar.setPosition(this.getPosition().x, this.getPosition().y);
+                    slowingTar.addEntityEffect(new EntityEffect(EntityEffect.EntityEffectType.DURATION, 600, 0, 0));
+                    this.getOwningScene().add(slowingTar, Layer.MAIN);
+                    
+                    
+                }
+                 
                  //remove from world
                 this.getBody().setVelocity(new Vector2f(0,0));
                 this.removeFromOwningScene();
@@ -121,6 +135,64 @@ public class EnemyRockThrow extends Skill
              
                 
          }
+    }
+    
+    
+    private class SlowingTarHitBox extends HitBox
+    {
+        
+        public SlowingTarHitBox(CombatEntity user)
+        {
+            super(new Damage(Damage.DamageType.NODAMAGE, 0), new Body(new Box(200,50), 4), new Image("black.png"),user); 
+           // this.getImage().setAlphaBrightness(.4f);
+            this.image.setDimensions(200, 50);
+            this.body.setGravityEffected(false);
+            this.body.removeExcludedBody(user.getBody());
+            this.getBody().setBitmask(BitMasks.NO_COLLISION.value);
+            this.getBody().setOverlapMask(OverlapMasks.OVERLAP_ALL.value);
+            
+            //add its praticle effects
+//            AbstractParticleEmitter emitter = new IceEmitter();
+//            emitter.setDuration(900);
+//            this.addEmitter(emitter);
+        }
+        
+        public void collidedWith(Entity other, CollisionEvent event)
+        {
+            //if we collided with enemy add damage
+            if(other instanceof PlayerEntity)
+            {
+                //remove finite effects
+                ((CombatEntity)other).getCombatData().removeCombatEffect("tarFinite");
+                
+                //apply slow
+                StateEffect slow = new StateEffect(StateEffect.StateEffectType.SLOW, 10, .50f, true);
+                slow.setInfinite();
+                ((CombatEntity)other).getCombatData().addCombatEffect("tarSlow", slow);
+                
+  
+                
+            }
+            
+        }
+        
+        public void separatedFrom(Entity other, CollisionEvent event)
+        {
+            if(other instanceof PlayerEntity)
+            {
+                //remove infinite effects 
+                ((CombatEntity)other).getCombatData().removeCombatEffect("tarSlow");
+                ((CombatEntity)other).getImage().setColor(new Color(1,1,1,1));
+    
+                
+                //apply finite slow
+                StateEffect slow = new StateEffect(StateEffect.StateEffectType.SLOW, 180, .50f, true);
+                ((CombatEntity)other).getCombatData().addCombatEffect("tarFinite", slow);
+            
+            }
+        }
+        
+
     }
 
 }
