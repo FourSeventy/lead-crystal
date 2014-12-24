5a7123556b4bbc6a0427d26b6acc8999aa45f054
Michael Signorella <michael@silvergobletgames.com>
2014-12-23 20:51:40 -0500

    tank missile attack

--- Source/src/com/silvergobletgames/leadcrystal/skills/EnemyHomingMissile.java
+++ Source/src/com/silvergobletgames/leadcrystal/skills/EnemyHomingMissile.java
@@ -57,59 +57,15 @@ public class EnemyHomingMissile extends Skill
         SylverVector2f distanceVector = user.distanceVector(user.getTarget());
         distanceVector.normalise();
         
-        // Bullet force
-        float xforce = 1800*distanceVector.x;
-        float yforce = 1800*distanceVector.y;
-
+         MissileProducer producer = new MissileProducer(damage,user.distanceVector(user.getTarget()),user.getTarget().getPosition(),user,origin);
+       producer.setPosition(origin.getX(), origin.getY());
        
-        //Dispense goo into the world
+       user.getOwningScene().add(producer, Layer.MAIN); 
+       
         float theta = (float)Math.acos(distanceVector.dot(new SylverVector2f(1,0)));
-        if(user.getTarget().getPosition().y < user.getPosition().y)
-            theta = (float)(2* Math.PI - theta);
-        
-        //body and image
-        Body body = new Body(new Box(50,30), 1);
-        Image img = new Image("rocket.png");
-        img.setDimensions(40, 20);
-         
-        //construct hitbox
-        EnemyRocketHitbox rocket = new EnemyRocketHitbox(damage, body, img, user);
-        
-       
-        //set rocket image
-        rocket.getBody().setRotation((float)theta);
-        rocket.getImage().setAngle((float)(theta * (180f/Math.PI))); 
-        
-        //add smoke effect
-        AbstractParticleEmitter rocketEmitter = new LeadCrystalParticleEmitters.RocketSmokeEmitter();
-        rocketEmitter.setAngle(180);
-        rocket.addEmitter(rocketEmitter);
-
-        //add rocket engine effect
-        Image flash = new Image("rocket_fire.png");
-        flash.setScale(.5f);
-        
-        //add image effect
-        Float[] points1 = {1f, 1.8f, 1f};
-        int[] durations1= {15,15};
-        ImageEffect flashEffect = new MultiImageEffect(ImageEffect.ImageEffectType.BRIGHTNESS, points1, durations1);
-        flashEffect.setRepeating(true);
-        flash.addImageEffect(flashEffect);
-        flash.setRotationPoint(1.4f, .5f);
-        flash.setAnchor(Anchorable.Anchor.RIGHTCENTER);
-        flash.setHorizontalFlip(false);
-        flash.setColor(new Color(2.5f,1.7f,1.7f,.45f));
-        flash.setAngle(rocket.getImage().getAngle()); 
-        Overlay o = new Overlay(flash, -1, new SylverVector2f(.1f,.5f));
-        o.setInfiniteDuration();
-        rocket.getImage().addOverlay(o);
-        
-
-      //Dispense rocket into the world
-        rocket.setPosition(origin.x + distanceVector.x * 25, origin.y + distanceVector.y * 25);
-        rocket.getBody().addForce(new Vector2f(xforce ,yforce));        
-        user.getOwningScene().add(rocket,Layer.MAIN);
-        
+           if(user.getTarget().getPosition().y < user.getPosition().y)
+               theta = (float)(2* Math.PI - theta);
+           
         //dispense muzzle flash
         Image muzzleFlash = new Image("flash.png");
         muzzleFlash.setScale(.14f);
@@ -139,6 +95,154 @@ public class EnemyHomingMissile extends Skill
         
     }
     
+    private class MissileProducer extends Entity
+    {
+        private long ticks = 0;
+        private SylverVector2f origin;
+        private Damage passthroughDamage;
+        private SylverVector2f vectorToTarget;
+        private SylverVector2f targetPosition;
+        private CombatEntity caster;
+        
+        
+        public MissileProducer(Damage passthroughDamage, SylverVector2f vectorToTarget,SylverVector2f targetPosition, CombatEntity caster,SylverVector2f origin)
+        { 
+           super( new Image("blank.png"),new Body(new Box(10,10), 1));
+           this.getBody().setGravityEffected(false);
+           this.getBody().setOverlapMask(Entity.OverlapMasks.NO_OVERLAP.value);
+           this.getBody().setBitmask(Entity.BitMasks.NO_COLLISION.value);
+           this.passthroughDamage = passthroughDamage;
+           this.vectorToTarget = vectorToTarget;
+           this.targetPosition = targetPosition;
+           this.caster = caster;
+           this.origin = origin;
+        }
+        
+        @Override
+        public void update()
+        {
+            this.ticks++;
+            
+            
+            if(this.ticks == 1)
+            {
+                
+                SylverVector2f vectorToTarget = new SylverVector2f(this.vectorToTarget);
+                vectorToTarget.normalise();
+                float degrees = Math.copySign(SylverRandom.random.nextInt(20), vectorToTarget.getX());
+                
+                SylverVector2f upVector = new SylverVector2f((float)(Math.cos(degrees * Math.PI/180) *vectorToTarget.x  - (Math.sin(degrees * Math.PI/180))*vectorToTarget.y ),(float)((Math.sin(degrees * Math.PI/180))*vectorToTarget.x  + (Math.cos(degrees * Math.PI/180))*vectorToTarget.y )); // 5 degreees
+                upVector.normalise();
+        
+                this.shootBullet(upVector, passthroughDamage, this.targetPosition,caster, origin);
+                
+               
+            }
+            else if(this.ticks == 15)
+            {
+                
+                 SylverVector2f vectorToTarget = new SylverVector2f(this.vectorToTarget);
+                vectorToTarget.normalise();
+                float degrees = Math.copySign(SylverRandom.random.nextInt(20), vectorToTarget.getX());
+                
+                SylverVector2f upVector = new SylverVector2f((float)(Math.cos(degrees * Math.PI/180) *vectorToTarget.x  - (Math.sin(degrees * Math.PI/180))*vectorToTarget.y ),(float)((Math.sin(degrees * Math.PI/180))*vectorToTarget.x  + (Math.cos(degrees * Math.PI/180))*vectorToTarget.y )); // 5 degreees
+                upVector.normalise();
+        
+                this.shootBullet(upVector, passthroughDamage, this.targetPosition, caster, origin);
+            }
+            else if(this.ticks == 30)
+            {
+                    
+                 SylverVector2f vectorToTarget = new SylverVector2f(this.vectorToTarget);
+                vectorToTarget.normalise();
+                float degrees = - Math.copySign(SylverRandom.random.nextInt(30), vectorToTarget.getX())  ;              
+                
+                SylverVector2f upVector = new SylverVector2f((float)(Math.cos(degrees * Math.PI/180) *vectorToTarget.x  - (Math.sin(degrees * Math.PI/180))*vectorToTarget.y ),(float)((Math.sin(degrees * Math.PI/180))*vectorToTarget.x  + (Math.cos(degrees * Math.PI/180))*vectorToTarget.y )); // 5 degreees
+                upVector.normalise();
+        
+                this.shootBullet(upVector, passthroughDamage, this.targetPosition,caster, origin);
+            }
+            else if (this.ticks == 45)
+            {
+                SylverVector2f vectorToTarget = new SylverVector2f(this.vectorToTarget);
+                vectorToTarget.normalise();
+                float degrees = - Math.copySign(SylverRandom.random.nextInt(30), vectorToTarget.getX())  ;              
+                
+                SylverVector2f upVector = new SylverVector2f((float)(Math.cos(degrees * Math.PI/180) *vectorToTarget.x  - (Math.sin(degrees * Math.PI/180))*vectorToTarget.y ),(float)((Math.sin(degrees * Math.PI/180))*vectorToTarget.x  + (Math.cos(degrees * Math.PI/180))*vectorToTarget.y )); // 5 degreees
+                upVector.normalise();
+        
+                this.shootBullet(upVector, passthroughDamage, this.targetPosition,caster, origin);
+            }
+            if(this.ticks > 180)
+            {
+                this.removeFromOwningScene();
+            }
+            
+            
+        }
+        
+        private void shootBullet(SylverVector2f vectorToTarget, Damage d, SylverVector2f targetPosition, CombatEntity caster, SylverVector2f origin)
+        {
+            //Damage is scaled with base
+            Damage damage = new Damage(d);
+
+           // Bullet force
+           float xforce = 1800*vectorToTarget.x;
+           float yforce = 1800*vectorToTarget.y;
+
+
+           //Dispense goo into the world
+           float theta = (float)Math.acos(vectorToTarget.dot(new SylverVector2f(1,0)));
+           if(user.getTarget().getPosition().y < user.getPosition().y)
+               theta = (float)(2* Math.PI - theta);
+
+           //body and image
+           Body body = new Body(new Box(50,30), 1);
+           Image img = new Image("rocket.png");
+           img.setDimensions(40, 20);
+
+           //construct hitbox
+           EnemyRocketHitbox rocket = new EnemyRocketHitbox(damage, body, img, user);
+
+
+           //set rocket image
+           rocket.getBody().setRotation((float)theta);
+           rocket.getImage().setAngle((float)(theta * (180f/Math.PI))); 
+
+           //add smoke effect
+           AbstractParticleEmitter rocketEmitter = new LeadCrystalParticleEmitters.RocketSmokeEmitter();
+           rocketEmitter.setAngle(180);
+           rocket.addEmitter(rocketEmitter);
+
+           //add rocket engine effect
+           Image flash = new Image("rocket_fire.png");
+           flash.setScale(.5f);
+
+           //add image effect
+           Float[] points1 = {1f, 1.8f, 1f};
+           int[] durations1= {15,15};
+           ImageEffect flashEffect = new MultiImageEffect(ImageEffect.ImageEffectType.BRIGHTNESS, points1, durations1);
+           flashEffect.setRepeating(true);
+           flash.addImageEffect(flashEffect);
+           flash.setRotationPoint(1.4f, .5f);
+           flash.setAnchor(Anchorable.Anchor.RIGHTCENTER);
+           flash.setHorizontalFlip(false);
+           flash.setColor(new Color(2.5f,1.7f,1.7f,.45f));
+           flash.setAngle(rocket.getImage().getAngle()); 
+           Overlay o = new Overlay(flash, -1, new SylverVector2f(.1f,.5f));
+           o.setInfiniteDuration();
+           rocket.getImage().addOverlay(o);
+
+
+         //Dispense rocket into the world
+           rocket.setPosition(origin.x + vectorToTarget.x * 25, origin.y + vectorToTarget.y * 25);
+           rocket.getBody().addForce(new Vector2f(xforce ,yforce));        
+           user.getOwningScene().add(rocket,Layer.MAIN); 
+        
+        }
+        
+        
+    }
     
      private static class EnemyRocketHitbox extends HitBox
     {
