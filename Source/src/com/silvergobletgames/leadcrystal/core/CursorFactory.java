5fbd8da9ec2d4c4ec339cc98142569838543b258
Michael Signorella <michael@silvergobletgames.com>
2015-01-20 12:20:39 -0500

    fixing viewport jostle in cave1

--- Source/src/com/silvergobletgames/leadcrystal/core/CursorFactory.java
+++ Source/src/com/silvergobletgames/leadcrystal/core/CursorFactory.java
@@ -51,7 +51,7 @@ public class CursorFactory
             case RETICLE: cursor = new Cursor(new Image("reticle06.png"){{ setDimensions(39,39); setColor(new Color(1f,1f,1f));}}, Anchorable.Anchor.CENTER);  break;
             case ACTIVEHAND: cursor = new Cursor(new Image("reticle08.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.CENTER); break;
             case UNACTIVEHAND: cursor = new Cursor(new Image("reticle10.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.CENTER);  break;
-            case POINTERHAND: cursor = new Cursor(new Image("reticle07.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.TOPCENTER); break;
+            case POINTERHAND: cursor = new Cursor(new Image("reticle07.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.TOPLEFT); break;
             case PURCHASEHAND: cursor = new Cursor(new Image("reticle09.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.CENTER); break;
             default: cursor = null;
         }
