21dc3253c672864c4a069003a1b6ffd7aed15933
Michael Signorella <michael@silvergobletgames.com>
2015-02-02 11:40:22 -0500

    centering cursors top center

--- Source/src/com/silvergobletgames/leadcrystal/core/CursorFactory.java
+++ Source/src/com/silvergobletgames/leadcrystal/core/CursorFactory.java
@@ -49,10 +49,10 @@ public class CursorFactory
         switch(type)
         {
             case RETICLE: cursor = new Cursor(new Image("reticle06.png"){{ setDimensions(39,39); setColor(new Color(1f,1f,1f));}}, Anchorable.Anchor.CENTER);  break;
-            case ACTIVEHAND: cursor = new Cursor(new Image("reticle08.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.CENTER); break;
-            case UNACTIVEHAND: cursor = new Cursor(new Image("reticle10.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.CENTER);  break;
+            case ACTIVEHAND: cursor = new Cursor(new Image("reticle08.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.TOPCENTER); break;
+            case UNACTIVEHAND: cursor = new Cursor(new Image("reticle10.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.TOPCENTER);  break;
             case POINTERHAND: cursor = new Cursor(new Image("reticle07.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.TOPLEFT); break;
-            case PURCHASEHAND: cursor = new Cursor(new Image("reticle09.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.CENTER); break;
+            case PURCHASEHAND: cursor = new Cursor(new Image("reticle09.png"){{ setDimensions(39,39);}}, Anchorable.Anchor.TOPCENTER); break;
             default: cursor = null;
         }
         
