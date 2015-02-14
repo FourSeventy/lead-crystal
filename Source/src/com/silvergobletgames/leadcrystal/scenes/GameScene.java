424552e8573548ac8442230e256612e6e63d1264
Michael Signorella <michael@silvergobletgames.com>
2015-02-13 20:28:41 -0500

    removed player physics hack

--- Source/src/com/silvergobletgames/leadcrystal/scenes/GameScene.java
+++ Source/src/com/silvergobletgames/leadcrystal/scenes/GameScene.java
@@ -108,7 +108,7 @@ public class GameScene extends Scene
         this.setViewport(view);        
         
         //build physics world
-        physicsWorld = new World(new Vector2f(0.0f, -57.0f), 7, new QuadSpaceStrategy(20, 5));
+        physicsWorld = new World(new Vector2f(0.0f, -57.0f), 5, new QuadSpaceStrategy(20, 5));
         physicsWorld.addListener(collisionHandler);
         physicsWorld.enableRestingBodyDetection(1f, 1f, 1f);
         
