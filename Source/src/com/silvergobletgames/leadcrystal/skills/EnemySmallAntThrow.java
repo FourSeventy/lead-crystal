2ed123fac786e769b92005436a7f96c14f764f73
Michael Signorella <michael@silvergobletgames.com>
2015-02-19 17:57:42 -0500

    fixing mass of thrown motoccos

--- Source/src/com/silvergobletgames/leadcrystal/skills/EnemySmallAntThrow.java
+++ Source/src/com/silvergobletgames/leadcrystal/skills/EnemySmallAntThrow.java
@@ -112,7 +112,7 @@ public class EnemySmallAntThrow extends Skill {
                  
                  Image antImage = new Image(new MoleAnimationPack());
                  antImage.setScale(.8f);
-                 Body antBody = new Body(new Circle(40),3);
+                 Body antBody = new Body(new Circle(40),10);
                  antBody.setFriction(0.3f);
                  antBody.setGravityEffected(true);
                  antBody.setOverlapMask(Entity.OverlapMasks.NPE.value);
