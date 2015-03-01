7dcf97b0a4430a3fe6c660a8660aeddf9ce11239
Michael Signorella <michael@silvergobletgames.com>
2015-03-01 00:24:03 -0500

    fixing a bug where you would open the map and immediately enter a level

--- Source/src/com/silvergobletgames/leadcrystal/menus/MapMenu.java
+++ Source/src/com/silvergobletgames/leadcrystal/menus/MapMenu.java
@@ -335,6 +335,7 @@ public class MapMenu extends Window{
         
         ((GameScene)this.getOwningScene()).getHud().closeDialogue();
         
+        Game.getInstance().getInputHandler().getInputSnapshot().killMouseClick();
         this.update();
        
     }
